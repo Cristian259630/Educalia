038379e372836a40cb25ac118f79448c89313626
Cristian259630 <fernandocristian294@gmail.com>
2024-07-15 03:58:29 -0600

    modificaciones

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploying-vite-project-example/",
+  base: "https://cristian259630.github.io/Educalia/",
   plugins: [
     vue(),
   ],
