99027df410c9940c62eda93648c4cb6991ed5fa5
Cristian259630 <fernandocristian294@gmail.com>
2024-07-21 00:01:33 -0600

    cambios hechos

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://github.com/Cristian259630/Educalia",
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://github.com/Cristian259630/Educalia",
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+module.exports = {
+  publicPath: ''
+}
